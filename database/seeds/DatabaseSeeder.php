38bbfdd5ac3e88da6ef61b7c38ec504320f5c8d2
Nasrul676 <nasrulmuhammad676@gmail.com>
2025-02-25 11:53:08 +0700

    delete: dupicated composer json

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $user = [
+
+            [
+               'id'=>'25769c6c-d34d-4bfe-ba98-e0ee856f3e7a',
+
+               'name'=>'Admin',
+
+               'email'=>'admin@gmail.com',
+
+                'is_admin'=>'admin',
+
+               'password'=> Hash::make('admin'),
+
+            ],
+
+            [
+              'id'=>'25769c6c-67jd-4bfe-ba98-343asfs8434',
+
+               'name'=>'Kasir',
+
+               'email'=>'kasir@gmail.com',
+
+                'is_admin'=>'kasir',
+
+               'password'=> Hash::make('kasir'),
+
+            ],
+
+        ];
+
+  
+
+        foreach ($user as $key => $value) {
+
+            User::create($value);
+
+        }
     }
 }
